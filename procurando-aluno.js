5846d363550e3e7ff4324c485bed1c55d3cb4ad2
bruno-ssilveira <129340450+bruno-ssilveira@users.noreply.github.com>
2023-06-05 11:52:07 -0300

    js_arrays

--- procurando-aluno.js
+++ procurando-aluno.js
@@ -5,15 +5,22 @@ const medias = [10, 8, 7.5, 9];
 const listaDeAlunosEMedias = [alunos, medias];
 
 function exibeNomeENota(aluno) {
+
     if(listaDeAlunosEMedias[0].includes(aluno)) {
+
+        const alunos = listaDeAlunosEMedias[0];
+        const medias = listaDeAlunosEMedias[1];
 
         console.log(`${aluno} está cadastrado`);
 
-        const indice = listaDeAlunosEMedias[0].indexOf(aluno);
+        const indice = alunos.indexOf(aluno);
 
         console.log(indice);
 
-        const media = listaDeAlunosEMedias[1][indice];
+        const mediaDoAluno = medias[1][indice];
+
+        console.log(`${aluno} tem a média ${mediaDoAluno}`)
+
     } else {
 
         console.log('Aluno não encontrado!');
@@ -21,3 +28,4 @@ function exibeNomeENota(aluno) {
 }
 
 exibeNomeENota('João');
+exibeNomeENota('Everaldo');
