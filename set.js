845167bb6bb204f62894c7b09bbb5e26f47b178e
bruno-ssilveira <129340450+bruno-ssilveira@users.noreply.github.com>
2023-06-06 13:59:04 -0300

    js_arrays

---
+++ set.js
@@ -0,0 +1,9 @@
+const nomes = ['Ana', 'Clara', 'Maria', 'Maria', 'João', 'João', 'João'];
+
+const nomesAtualizados = [...new Set(nomes)];
+
+// const meuSet = new Set(nomes);
+
+// const nomesAtualizados = [...meuSet];
+
+console.log(nomesAtualizados);
